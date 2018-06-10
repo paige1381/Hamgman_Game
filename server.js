746d1c4fe807483ab810ebfc4acf2d7747ff7c9c
Paige Boyer <paige1381@gmail.com>
2018-06-10 14:41:39 -0700

    correct letter displaying in blanks

--- server.js
+++ server.js
@@ -6,37 +6,51 @@ app.use(express.static('public'));
 
 
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let gameWords = ['RABBIT', 'BUNNY', 'CARROT', 'LETTUCE', 'BURROW', 'FLUFFY', 'FLOPPY', 'LITTER', 'PELLETS']
+let gameWords = ['RABBIT', 'BUNNY', 'CARROT', 'LETTUCE', 'BURROW', 'FLUFFY', 'FLOPPY', 'LITTER', 'PELLETS'];
 let currentWord = gameWords[Math.floor(Math.random() * (gameWords.length))].split("");
 let guesses = 8;
 let currentLetter = null;
 let guessWord = null;
 let correctLetters = [];
+let allLetters = [];
 
 console.log(currentWord);
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     alphabet: alphabet,
+    gameWords: gameWords,
     currentWord: currentWord,
     guesses: 8,
-    currentLetter: currentLetter,
+    currentLetter: null,
     guessWord: guessWord,
-    correctLetters: correctLetters
+    correctLetters: [],
+    allLetters: []
   });
   console.log(currentWord);
 });
 
 app.get('/:id', (req, res) => {
+  currentLetter = alphabet[req.params.id];
+  if (currentWord.indexOf(currentLetter) > -1) {
+    correctLetters.push(currentLetter);
+  };
+  correctLetters = [...new Set(correctLetters)];
   guesses--;
+  console.log(guesses);
   res.render('index.ejs', {
     alphabet: alphabet,
+    gameWords: gameWords,
     currentWord: currentWord,
     guesses: guesses,
-    currentLetter: alphabet[req.params.id],
+    currentLetter: currentLetter,
     guessWord: guessWord,
-    correctLetters: correctLetters
+    correctLetters: correctLetters,
+    allLetters: allLetters.push(currentLetter),
   });
+  console.log(guesses);
+  console.log('currentLetter:', currentLetter);
+  console.log('allLetters:', allLetters);
 });
 
 app.listen(PORT, console.log('listening on port', PORT));
