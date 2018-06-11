ef0d8da0a9c45f86b1d8d6bc58e531e12de92e75
Paige Boyer <paige1381@gmail.com>
2018-06-10 18:10:33 -0700

    added multiple rounds

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('public'));
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let gameWords = ['RABBIT', 'BUNNY', 'CARROT', 'LETTUCE', 'BURROW', 'FLUFFY', 'FLOPPY', 'LITTER', 'PELLETS'];
 let currentWord = gameWords[Math.floor(Math.random() * (gameWords.length))].split("");
-let guesses = 8;
 let currentLetter = null;
+let guesses = 8;
 let guessWord = null;
 let correctLetters = [];
 let allLetters = [];
@@ -27,8 +27,44 @@ app.get('/', (req, res) => {
     correctLetters: [],
     allLetters: []
   });
-  console.log(currentWord);
+  console.log('--- new game ---');
+  console.log('gameWords:', gameWords);
+  console.log('currentWord:', currentWord);
+  console.log('guesses:', guesses);
+  console.log('currentLetter:', currentLetter);
+  console.log('correctLetters:', correctLetters);
+  console.log('allLetters:', allLetters);
 });
+
+
+app.get('/newRound', (req, res) => {
+  let index = gameWords.indexOf(currentWord.join(""));
+  gameWords.splice(index, 1);
+  currentWord = gameWords[Math.floor(Math.random() * (gameWords.length))].split("");
+  guesses = 8;
+  currentLetter = [];
+  correctLetters = [];
+  allLetters = [];
+  res.render('index.ejs', {
+    alphabet: alphabet,
+    gameWords: gameWords,
+    currentWord: currentWord,
+    guesses: guesses,
+    currentLetter: currentLetter,
+    guessWord: guessWord,
+    correctLetters: correctLetters,
+    allLetters: allLetters
+  });
+  console.log('--- new round ---');
+  console.log('gameWords:', gameWords);
+  console.log('currentWord:', currentWord);
+  console.log('guesses:', guesses);
+  console.log('currentLetter:', currentLetter);
+  console.log('correctLetters:', correctLetters);
+  console.log('allLetters:', allLetters);
+});
+
+
 
 app.get('/:id', (req, res) => {
   currentLetter = alphabet[req.params.id];
@@ -47,8 +83,21 @@ app.get('/:id', (req, res) => {
     correctLetters: correctLetters,
     allLetters: allLetters.push(currentLetter),
   });
+  console.log('--- new turn ---');
+  console.log('gameWords:', gameWords);
+  console.log('currentWord:', currentWord);
+  console.log('guesses:', guesses);
   console.log('currentLetter:', currentLetter);
+  console.log('correctLetters:', correctLetters);
   console.log('allLetters:', allLetters);
 });
+
+
+
+
+
+
+
+
 
 app.listen(PORT, console.log('listening on port', PORT));
